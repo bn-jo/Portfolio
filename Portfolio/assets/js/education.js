5a78462917860f1c46d9784e985d31c9ebb797bb
Ben Guetta <95125332+bn-jo@users.noreply.github.com>
2022-02-23 00:34:09 +0200

    Update education.js

--- Portfolio/assets/js/education.js
+++ Portfolio/assets/js/education.js
@@ -135,22 +135,22 @@ document.addEventListener("DOMContentLoaded", showCards);
 
 /* Timeline Section*/
 
-$(function(){
+// $(function(){
 
-  window.sr = ScrollReveal();
+//   window.sr = ScrollReveal();
 
-  if ($(window).width() < 768) {
+//   if ($(window).width() < 768) {
 
-    if ($('.timeline-content').hasClass('js--fadeInLeft')) {
-      $('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
-    }
+//     if ($('.timeline-content').hasClass('js--fadeInLeft')) {
+//       $('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
+//     }
 
-    sr.reveal('.js--fadeInRight', {
-      origin: 'right',
-      distance: '300px',
-      easing: 'ease-in-out',
-      duration: 800,
-    });
+//     sr.reveal('.js--fadeInRight', {
+//       origin: 'right',
+//       distance: '300px',
+//       easing: 'ease-in-out',
+//       duration: 800,
+//     });
 
   } else {
     
